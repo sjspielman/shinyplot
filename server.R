4ab6aaa5efdfbe44ee0a2d98717da483bafd988d
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2018-09-09 13:59:08 -0400

    More modular code and use of tidy eval instead of aes_string. Also solves bug of spaces in col names

--- server.R
+++ server.R
@@ -40,10 +40,10 @@ server <- function(input, output) {
         switch(input$dataviz, 
                 "quant" = selectInput("whichquant", 
                         "Which type of plot would you like to make?", 
-                        choices = c("Histogram", "Density plot", "Boxplot", "Jitter plot", "Make all")),
+                        choices = c("Histogram", "Density plot", "Boxplot", "Jitter plot", "Bar plot", "Make all")),
                 "multquant" = selectInput("whichquant", 
                         "Which type of plot would you like to make?", 
-                        choices = c("Boxplot", "Density plot", "Jitter plot", "Make all"))
+                        choices = c("Boxplot", "Density plot", "Jitter plot", "Bar plot", "Line plot", "Make all"))
                 )
         ####### to do: we should be able to do this with barplots and error bars ########    
     })
@@ -86,106 +86,194 @@ server <- function(input, output) {
     })
 
 
-    ### "Regular" function which returns the plot
-    zeplot <- function() {
 
-        finaldata <- isolate(userdata()) 
+
+    ################################################################################################
+    ############################### Functions for making individual plots ##########################
+    isolate_data <- function(){
+        isolate(userdata()) 
+    }        
+
+
+    isolate_color <- function(){
+        isolate(input$yaycolor)
+    }
+        plot_histogram <- function()
+    {
+        finaldata <- isolate_data()
+        thecolor  <- isolate_color()
+        quantvar2 <- as.symbol( isolate(input$quantvar) )
+        p <- ggplot(finaldata, aes(x = !!quantvar2)) + geom_histogram(color = "black", fill = thecolor)
+        p    
+    }
+
+    plot_density <- function()
+    {
+        finaldata <- isolate_data()
+        thecolor  <- isolate_color()
+        quantvar2 <- as.symbol( isolate(input$quantvar) )
+        p <- ggplot(finaldata, aes(x = !!quantvar2)) + geom_density(color = "black", fill = thecolor, alpha=0.8)
+        p    
+    }
+    plot_boxplot <- function()
+    {
+        finaldata <- isolate_data()
+        thecolor  <- isolate_color()
+        quantvar2 <- as.symbol( isolate(input$quantvar) )
+        p <- ggplot(finaldata, aes(y = !!quantvar2)) + geom_boxplot(fill = thecolor) + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
+        p    
+    }
+
+    plot_jitter <- function()
+    {
+        finaldata <- isolate_data()
+        thecolor  <- isolate_color()
+        quantvar2 <- as.symbol( isolate(input$quantvar) )
+        p <- ggplot(finaldata, aes(y = !!quantvar2)) + geom_jitter(aes(x=""), color = thecolor, width=0.075) + xlab("")
+        p    
+    }
+    plot_barquant <- function()
+    {
+        finaldata <- isolate_data()
+        thecolor  <- isolate_color()
+        quantvar2 <- as.symbol( isolate(input$quantvar) )
+        p <- ggplot(finaldata, aes(x="")) + stat_summary(aes(y = !!quantvar2), fun.y = "mean", geom = "bar", width=0.1, fill = thecolor) + stat_summary(aes(y = !!quantvar2), fun.dat = "mean_se", geom = "errorbar", width = 0.05) + xlab("")
+        p    
+    }
+
+    plot_multbox <- function()
+    {
+        finaldata <- isolate_data()
+        quantvar2 <-  as.symbol(isolate(input$quantvar))
+        quantvar_cat2 <-  as.symbol(isolate(input$quantvar_cat))
+        p <- ggplot(finaldata, aes(x = !!quantvar_cat2, y = !!quantvar2, group = !!quantvar_cat2, fill = factor(!!quantvar_cat2))) + geom_boxplot() + scale_fill_hue(name = quantvar_cat2, l=45)
+        p
+    }
+    plot_multdensity <- function()
+    {
+        finaldata <- isolate_data()
+        quantvar2 <-  as.symbol(isolate(input$quantvar))
+        quantvar_cat2 <-  as.symbol(isolate(input$quantvar_cat))
+        p <- ggplot(finaldata, aes(x = !!quantvar2, fill = factor(!!quantvar_cat2))) + geom_density(color = "black", alpha=0.5) + scale_fill_hue(name = quantvar_cat2, l=45)
+        p
+    }
+    plot_multjitter <- function()
+    {
+        finaldata <- isolate_data()
+        quantvar2 <-  as.symbol(isolate(input$quantvar))
+        quantvar_cat2 <-  as.symbol(isolate(input$quantvar_cat))
+        p <- ggplot(finaldata, aes(x = !!quantvar_cat2, y = !!quantvar2, group = !!quantvar_cat2, color = factor(!!quantvar_cat2))) + geom_jitter(width = 0.1) + scale_color_hue(name = quantvar_cat2, l=45)
+        p
+    }
+    plot_multbarquant <- function()
+    {
+        finaldata <- isolate_data()
+        quantvar2 <-  as.symbol(isolate(input$quantvar))
+        quantvar_cat2 <-  as.symbol(isolate(input$quantvar_cat))
+        p <- ggplot(finaldata) + stat_summary_bin(aes(x = factor(!!quantvar_cat2), y = !!quantvar2, fill = factor(!!quantvar_cat2)), fun.y = "mean", geom = "bar", width=0.1) + stat_summary_bin(aes(factor(!!quantvar_cat2), y = !!quantvar2), fun.data = "mean_se", geom = "linerange", size=1.5) + xlab(quantvar_cat2) +  scale_fill_hue(name = quantvar_cat2, l=45)
+        p
+    }
+    plot_line <- function()
+    {
+        finaldata <- isolate_data()
+        quantvar2 <-  as.symbol(isolate(input$quantvar))
+        quantvar_cat2 <-  as.symbol(isolate(input$quantvar_cat))
+        p <- ggplot(finaldata) + 
+                stat_summary_bin(aes(x = factor(!!quantvar_cat2), y = !!quantvar2, color = factor(!!quantvar_cat2)), fun.y = "mean", geom = "point", size=5) +
+                stat_summary_bin(aes(x = factor(!!quantvar_cat2), y = !!quantvar2), group=1, fun.y = "mean", geom = "line") +
+                stat_summary_bin(aes(factor(!!quantvar_cat2), y = !!quantvar2), fun.data = "mean_se", geom = "linerange") + xlab(quantvar_cat2) +  scale_color_hue(name = quantvar_cat2, l=45) 
+        p
+    }
+
+    plot_barcount <- function()
+    {
+        finaldata <- isolate_data()
+        thecolor  <- isolate_color()
+        countvar <-  as.symbol(isolate(input$countvar))
+        p <- ggplot(finaldata, aes(x = factor(!!countvar))) + geom_bar(fill = thecolor) + xlab(countvar)
+        p
+    }
+    
+    plot_scatter <- function()
+    {
+        finaldata <- isolate_data()
+        thecolor  <- isolate_color()
+        x <-  as.symbol(isolate(input$xvar))
+        y <-  as.symbol(isolate(input$yvar))
+        regression <- isolate(input$bestfit)
+        p <- ggplot(finaldata, aes(x = !!x, y = !!y)) + geom_point()
+        if (regression == TRUE) 
+        {
+            p <- p + geom_smooth(color = thecolor, method = "lm")
+        }
+        p
+    }
+    ################################################################################################
+    ################################################################################################
+    
+
+    
+    ### "Regular" function which returns the plot
+    printplot <- function() {
+
         viz <- isolate(input$dataviz)
         geom_quant <- isolate(input$whichquant)
-        thecolor <- isolate(input$yaycolor)
-
 
         ################ Single distribution ##################
-        if (viz == "quant" & geom_quant == "Histogram")
-        {
-            quantvar2 <- isolate(input$quantvar)
-            p <- ggplot(finaldata, aes_string(x = quantvar2)) + geom_histogram(color = "black", fill = thecolor)
-        }
-        if (viz == "quant" & geom_quant == "Density plot")
-        {
-            quantvar2 <- isolate(input$quantvar)
-            p <- ggplot(finaldata, aes_string(x = quantvar2)) + geom_density(color = "black", fill = thecolor, alpha=0.8)
-        }
-        if (viz == "quant" & geom_quant == "Boxplot")
-        {
-            quantvar2 <- isolate(input$quantvar)
-            p <- ggplot(finaldata, aes_string(y = quantvar2)) + geom_boxplot(fill = thecolor) + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
-        }
-        if (viz == "quant" & geom_quant == "Jitter plot")
-        {
-            quantvar2 <- isolate(input$quantvar)
-            p <- ggplot(finaldata, aes_string(y = quantvar2)) + geom_jitter(aes(x=""), color = thecolor, width=0.075) + xlab("")
-        }
+        if (viz == "quant" & geom_quant == "Histogram") p <- plot_histogram()
+
+        if (viz == "quant" & geom_quant == "Density plot") p <- plot_density()
+
+        if (viz == "quant" & geom_quant == "Boxplot") p <- plot_boxplot()
+
+        if (viz == "quant" & geom_quant == "Jitter plot") p <- plot_jitter()
+
+        if (viz == "quant" & geom_quant == "Bar plot") p <- plot_barquant()
+
         if (viz == "quant" & geom_quant == "Make all")
         {
-            quantvar2 <- isolate(input$quantvar)
-            p1 <- ggplot(finaldata, aes_string(x = quantvar2)) + geom_histogram(color = "black", fill = thecolor)
-            p2 <- ggplot(finaldata, aes_string(x = quantvar2)) + geom_density(color = "black", fill = thecolor, alpha=0.8)
-            p3 <- ggplot(finaldata, aes_string(y = quantvar2)) + geom_boxplot(fill = thecolor)
-            p4 <- ggplot(finaldata, aes_string(y = quantvar2)) + geom_jitter(aes(x=""), color = thecolor, width=0.075) + xlab("")
-            p <- (p1 + p2) / (p3 + p4)
+            p1 <- plot_histogram()
+            p2 <- plot_density()
+            p3 <- plot_boxplot()
+            p4 <- plot_jitter()
+            p5 <- plot_barquant()
+            
+            p <- (p1 + p2 + p3) / (p4 + p5)
         }
         ########################################################
 
         ################ Multiple distributions ##################
-        if (viz == "multquant" & geom_quant == "Boxplot")
-        {
-            quantvar2 <- isolate(input$quantvar)
-            quantvar_cat2 <- isolate(input$quantvar_cat)
+        if (viz == "multquant" & geom_quant == "Boxplot") p <- plot_multbox()
 
-            #finaldata[,quantvar_cat2] <- factor(finaldata[,quantvar_cat2]) # doesn't work, why?!
-            p <- ggplot(finaldata, aes_string(x = quantvar_cat2, y = quantvar2, group = quantvar_cat2, fill = paste0("factor(",quantvar_cat2,")"))) + geom_boxplot() + scale_fill_hue(name = quantvar_cat2, l=45)
-        }
-        if (viz == "multquant" & geom_quant == "Density plot")
+        if (viz == "multquant" & geom_quant == "Density plot") p <- plot_multdensity()
+
+        if (viz == "multquant" & geom_quant == "Jitter plot") p <- plot_multjitter()
+
+        if (viz == "multquant" & geom_quant == "Bar plot") p <- plot_multbarquant()
+
+        if (viz == "multquant" & geom_quant == "Line plot") p <- plot_line()
+
+        if (viz == "multquant" & geom_quant == "Make all")
         {
-            quantvar2 <- isolate(input$quantvar)
-            quantvar_cat2 <- isolate(input$quantvar_cat)
-            p <- ggplot(finaldata, aes_string(x = quantvar2, fill = paste0("factor(",quantvar_cat2,")"))) + geom_density(color = "black", alpha=0.5) + scale_fill_hue(name = quantvar_cat2, l=45)
-        }
-        if (viz == "multquant" & geom_quant == "Jitter plot") ## todo: color by group such that it forces factor in aes_string?
-        {
-            quantvar2 <- isolate(input$quantvar)
-            quantvar_cat2 <- isolate(input$quantvar_cat)
-            p <- ggplot(finaldata, aes_string(x = quantvar_cat2, y = quantvar2, group = quantvar_cat2, color = paste0("factor(",quantvar_cat2,")") )) + geom_jitter(width = 0.1) + theme(legend.position = "none") + scale_fill_hue(name = quantvar_cat2, l=45)
-        }
-        if (viz == "multquant" & geom_quant == "Make all") 
-        {
-            quantvar2 <- isolate(input$quantvar)
-            quantvar_cat2 <- isolate(input$quantvar_cat)
-            p1 <- ggplot(finaldata, aes_string(x = quantvar_cat2, y = quantvar2, fill = paste0("factor(",quantvar_cat2,")"), group = quantvar_cat2)) + geom_boxplot() + theme(legend.position = "none") + scale_fill_hue(l=45)
-            p2 <- ggplot(finaldata, aes_string(x = quantvar2, fill = paste0("factor(",quantvar_cat2,")") )) + geom_density(color = "black", alpha=0.6) + theme(legend.position = "bottom") + scale_fill_hue(l=45, name = quantvar_cat2)
-            p3 <- ggplot(finaldata, aes_string(x = quantvar_cat2, y = quantvar2, group = quantvar_cat2, color = paste0("factor(",quantvar_cat2,")") )) + geom_jitter(width = 0.1) + theme(legend.position = "none") + scale_color_hue(l=45)
-            p <- p1 + p2 + p3
+            p1 <- plot_multbox() + theme(legend.position = "none")
+            p2 <- plot_multdensity() + theme(legend.position = "none")
+            p3 <- plot_multjitter() + theme(legend.position = "none")
+            p4 <- plot_multbarquant() + theme(legend.position = "bottom")
+            p5 <- plot_line() + theme(legend.position = "none")
+            
+            p <- (p1+p2+p3)/(p4+p5)
         }
         ########################################################
 
 
-        ################ Barplot for counts ##################
-        if (viz == "counts")
-        {
-            countvar <- isolate(input$countvar)
-    
-            p <- ggplot(finaldata, aes_string(x = countvar)) + geom_bar(fill = thecolor)
-        }
+        ################ Barplot for counts ####################
+        if (viz == "counts") p <- plot_barcount()
         ########################################################
 
-        ################ Scatterplot ##################
-        if (viz == "scatter")
-        {
-            x <- isolate(input$xvar)
-            y <- isolate(input$yvar)
-            regression <- isolate(input$bestfit)
-            if (regression == TRUE) 
-            {
-                p <- ggplot(finaldata, aes_string(x = x, y = y)) + geom_point() + geom_smooth(color = thecolor, method = "lm")
-            }
-            else 
-            {
-                p <- ggplot(finaldata, aes_string(x = x, y = y)) + geom_point()
-
-            }
-        }
+        ################ Scatterplot ###########################
+        if (viz == "scatter") p <- plot_scatter()
         ########################################################
+        
         print(p)
     }
 
@@ -195,7 +283,7 @@ server <- function(input, output) {
 
     
         output$mahplot <- renderPlot( { 
-            zeplot()
+            printplot()
         })
 
         output$download <- renderUI({
@@ -208,7 +296,7 @@ server <- function(input, output) {
                 "download.png"
             },
             content = function(file) {
-                ggsave(file, zeplot(), width = 6, height = 4)
+                ggsave(file, printplot(), width = 6, height = 4)
             }
         )
     })  
